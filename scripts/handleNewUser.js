189daa0c16a557518a6bf66e39d4feef6a5c2cd1
jeko <jemali.ergemlidze.1@btu.edu.ge>
2023-03-25 00:32:24 +0400

    cc

--- scripts/handleNewUser.js
+++ scripts/handleNewUser.js
@@ -1,39 +1,54 @@
+
 var nameValue = document.querySelector(".name")
 var surnameValue = document.querySelector(".surname")
 var addButton = document.querySelector(".sbuser")
 
 var arr = JSON.parse(localStorage.getItem("data")) || []
 
+
+
+var onlyLetters = /^[a-zA-Z]+$/
+
+
+
+
 const handleSubmit = (e) => {
-    
+
+
     var result = {
-        name:"",
-        surname:"",
-        id:Math.random() * 10000
+        name: "",
+        surname: "",
+        id: Math.random() * 10000
     }
+
+
 
     result.name = nameValue.value
     result.surname = surnameValue.value
 
-    arr.push(result)
 
-    localStorage.setItem("data",JSON.stringify(arr))
-    // console.log([...arr,result])
-    
+
+    location.reload()
+    if (onlyLetters.test(result.name)) {
+        arr.push(result)
+        localStorage.setItem("data", JSON.stringify(arr))
+
+    }
+
 }
 const removeItems = (id) => {
-    arr = arr.filter((item)=> {
+    arr = arr.filter((item) => {
         return item.id != id
     })
-    localStorage.setItem("data",JSON.stringify(arr))
+    localStorage.setItem("data", JSON.stringify(arr))
     location.reload()
 }
 
-addButton.addEventListener("click",(e)=> {
+addButton.addEventListener("click", (e) => {
     handleSubmit(e)
-    
+
     // save to localstorage
-    
+
     // localStorage.setItem("data",JSON.stringify(result))
 })
 
