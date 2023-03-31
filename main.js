ec052827a7a85e3fd63e25925813f58f35a8078d
jeko <jemali.ergemlidze.1@btu.edu.ge>
2023-03-31 11:15:32 +0400

    cc

--- main.js
+++ main.js
@@ -1,31 +1,31 @@
-const names = ["gocha", "nika", "zviadi", "nugzari", "lela", "maka", "mariami", "lola", "shalva", "gurjistani", "jeko", "gia", "ucha"]
-const surnames = ["chkadua", "akhmeteli", "zviadauri", "ergemlidze", "samanishvili", "rustaveli", "gociridze", "alania", "gorgadze"]
-var student = [false, true]
-var full = []
+// const names = ["gocha", "nika", "zviadi", "nugzari", "lela", "maka"]
+// const surnames = ["chkadua", "akhmeteli", "zviadauri", "ergemlidze", "samanishvili"]
+// var student = [false, true]
+// var full = []
 
-for (var i = 0; i < names.length; i++) {
-    for (var k = 0; k < names.length; k++) {
-        full.push(
-            {
-                name: names[i],
-                surname: surnames[k],
-                student: student[Math.floor(Math.random() * 2)],
-                id: Math.floor(Math.random() * 1000000)
-            }
-        )
-    }
-}
-function shuffleArray(array) {
-    for (var i = array.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    }
-}
+// for (var i = 0; i < names.length; i++) {
+//     for (var k = 0; k < names.length; k++) {
+//         full.push(
+//             {
+//                 name: names[i],
+//                 surname: surnames[k],
+//                 student: student[Math.floor(Math.random() * 2)],
+//                 id: Math.floor(Math.random() * 1000000)
+//             }
+//         )
+//     }
+// }
+// function shuffleArray(array) {
+//     for (var i = array.length - 1; i > 0; i--) {
+//         var j = Math.floor(Math.random() * (i + 1));
+//         var temp = array[i];
+//         array[i] = array[j];
+//         array[j] = temp;
+//     }
+// }
 
-shuffleArray(full)
+// shuffleArray(full)
 
-if (!localStorage.getItem("data")) {
-    localStorage.setItem("data", JSON.stringify(full))
-}
+// if (!localStorage.getItem("data")) {
+//     localStorage.setItem("data", JSON.stringify(full))
+// }
