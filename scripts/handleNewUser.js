06b3f041c545b2ce5b5cce6463567d45f71a0ab1
jeko <jemali.ergemlidze.1@btu.edu.ge>
2023-04-04 15:38:07 +0400

    handleNewUser.js Modified

--- scripts/handleNewUser.js
+++ scripts/handleNewUser.js
@@ -2,11 +2,12 @@
 var nameValue = document.querySelector(".name")
 var ageValue = document.querySelector(".age")
 var surnameValue = document.querySelector(".surname")
+
 var addButton = document.querySelector(".sbuser")
 var errorM = document.querySelector(".error")
 var arr = JSON.parse(localStorage.getItem("data")) || []
 var student = document.getElementById("student")
-var forbidenVars = ['!','@','#','$','%','^','&','*','(',')']
+var forbidenVars = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')']
 
 errorM.classList.add("d-none")
 
@@ -20,57 +21,60 @@ const handleError = (errorMessage) => {
 
 var validate = true
 
+class NewUser {
+    constructor(name, surname, student, age, id) {
+        this.name = name
+        this.student = student
+        this.surname = surname
+        this.age = age
+        this.id = id
+    }
+
+    addNewUser() {
+        
+        if (this.name.length <= 1 || this.surname.length <= 3) {
+            handleError("name or surname must be longer than 3 characters")
+            validate = false
+            setTimeout(() => {
+                handleError("")
+                errorM.classList.add("d-none")
+                validate = true
+            }, 2000)
+        }
+        forbidenVars.map((char) => {
+            if (this.name.includes(char) || this.surname.includes(char)) {
+
+                handleError("invalid characters")
+                setTimeout(() => {
+                    handleError("")
+                    errorM.classList.add("d-none")
+                }, 2000)
+                validate = false
+            }
+        })
+
+        // push to localstorage
+        if (validate) {
+            arr.push(this)
+            localStorage.setItem("data", JSON.stringify(arr))
+            location.reload()
+        }
+    }
+}
+
 
 const handleSubmit = (e) => {
     e.preventDefault();
 
-    var result = {
-        name: "",
-        student:null,
-        surname: "",
-        age:0,
-        id: Math.floor(Math.random() * 10000000)
-    }
-
-    console.log(ageValue.value)
-
-    result.name = nameValue.value
-    result.surname = surnameValue.value
-    result.age = Number(ageValue.value)
-    result.student = student.checked ? true : false
-
-
-    // validation 
+    var myNewUser = new NewUser(
+        nameValue.value,
+        surnameValue.value,
+        student.checked ? true : false,
+        Number(ageValue.value),
+        Math.floor(Math.random() * 9999999)
+    )
     
-    if (result.name.length <= 1 || result.surname.length <= 3 ) {
-        handleError("name or surname must be longer than 3 characters")
-        validate = false
-        setTimeout(()=> {   
-            handleError("")
-            errorM.classList.add("d-none")
-            validate = true
-        },2000)
-    } 
-    forbidenVars.map((char)=> {
-        if (result.name.includes(char) || result.surname.includes(char)) {
-            handleError("invalid characters")
-            setTimeout(()=> {
-                handleError("")
-                errorM.classList.add("d-none")
-            },2000)
-            validate = false
-        }
-    })
-
-
-    // push to localstorage
-    if (validate) {
-        arr.push(result)
-        localStorage.setItem("data", JSON.stringify(arr))
-        location.reload()
-    } 
-
-
+    myNewUser.addNewUser()
 
 }
 
@@ -83,17 +87,11 @@ const removeItems = (ID) => {
 
     localStorage.setItem("data", JSON.stringify(arr))
 
-    console.log(arr)
-
     location.reload()
 }
 
 addButton.addEventListener("click", (e) => {
     handleSubmit(e)
-
-    // save to localstorage
-
-    // localStorage.setItem("data",JSON.stringify(result))
 })
 
 export default removeItems
